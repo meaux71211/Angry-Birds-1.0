c7e19622328c47f73e68b9e4abe43b1fb0ef8bd1
meaux71211 <64816206+meaux71211@users.noreply.github.com>
2022-07-31 13:34:26 -0400

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box {
+    constructor(x,y,w,h) {
+        var options = {
+            restitution: 0.8,
+            friction: 0.3,
+            density: 1.0,
+        }
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.w = w;
+        this.h = h;
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        strokeWeight(4);
+        stroke("green");
+        fill("white");
+        rectMode(CENTER);
+        rect(0,0,this.w,this.h);
+        pop();
+    }
+}
